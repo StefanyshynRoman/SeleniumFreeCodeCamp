a765bc89443c38e846ea847c1a4aee16bdfdc3ee
roman <romanpz051@gmail.com>
2026-03-15 15:27:21 +0100

    0.0.1.0 Create Page Object Model

---
+++ src/main/java/com/saucedemo/pages/BasePage.java
@@ -0,0 +1,30 @@
+package com.saucedemo.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+/**
+ * @author romanpz051@gmail.com on 15.03.2026.
+ * @project SeleniumFreeCodeCamp
+ */
+public class BasePage {
+    public static WebDriver driver;
+
+    public void setDriver(WebDriver driver) {
+        BasePage.driver = driver;
+    }
+
+    protected WebElement find(By locator) {
+        return driver.findElement(locator);
+    }
+
+    protected void set(By locator, String text) {
+        find(locator).clear();
+        find(locator).sendKeys(text);
+    }
+    protected void click(By locator) {
+        find(locator).click();
+    }
+}
---
+++ src/main/java/com/saucedemo/pages/LoginPage.java
@@ -0,0 +1,34 @@
+package com.saucedemo.pages;
+
+import org.openqa.selenium.By;
+
+/**
+ * @author romanpz051@gmail.com on 15.03.2026.
+ * @project SeleniumFreeCodeCamp
+ */
+public class LoginPage  extends BasePage{
+    private By usernameField =By.id("user-name");
+    private By passwordField =By.id("password");
+    private By loginButton=By.id("login-button");
+    private By errorMessage=By.xpath("#login_button_container h3");
+
+    public void setUsername(String username) {
+        set(usernameField,username);
+    }
+    public void setPassword(String password) {
+        set(passwordField,password);
+    }
+    public ProductsPage clickLoginButton() {
+        click(loginButton);
+        return new ProductsPage();
+    }
+
+    public ProductsPage loginIntoApplication(String username, String password) {
+        setUsername(username);
+        setPassword(password);
+        return clickLoginButton();
+    }
+    public String getErrorMessage() {
+        return find(errorMessage).getText();
+    }
+}
