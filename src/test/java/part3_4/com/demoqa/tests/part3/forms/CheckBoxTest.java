2916b1da52f964ed8cb9d4b5182abd1bb2843df6
roman <romanpz051@gmail.com>
2026-04-20 17:21:15 +0200

    0.0.2.4 Working With Checkbox

--- src/test/java/part3_4/com/demoqa/tests/part3/forms/CheckBoxTest.java
+++ src/test/java/part3_4/com/demoqa/tests/part3/forms/CheckBoxTest.java
@@ -1,7 +1,22 @@
 package part3_4.com.demoqa.tests.part3.forms;
 
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
 import part3_4.com.demoqa.base.BaseTest;
 
 public class CheckBoxTest extends BaseTest {
+    @Test
+    public void checkboxTest(){
+        var formsPage=homePage.goToForms().clickPracticeForm();
+        formsPage.clickSportsCheckbox();
+        formsPage.clickReadingCheckbox();
+        formsPage.clickMusicCheckbox();
+        formsPage.unclickReadingCheckbox();
+
+       boolean isReadingCheckboxSelected= formsPage.isReadingSelected();
+        Assert.assertFalse(isReadingCheckboxSelected,
+                "\n Reading Checkbox Is Selected \n");
+
+    }
 }
