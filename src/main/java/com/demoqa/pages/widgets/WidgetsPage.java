1cd741a79fec65a7fb4f629198f30dd58efabae4
roman <romanpz051@gmail.com>
2026-05-24 15:33:17 +0200

    0.0.2.95 Working With Dates  test

--- src/main/java/com/demoqa/pages/widgets/WidgetsPage.java
+++ src/main/java/com/demoqa/pages/widgets/WidgetsPage.java
@@ -8,6 +8,8 @@ import static utilities.JavaScriptUtility.scrollToElementJS;
 
 public class WidgetsPage extends HomePage {
     private By selectMenuItem = By.xpath("//li[@id='item-8']//span[text()='Select Menu']");
+    private By datePickerMenuItem= By.xpath("//li[@id='item-2']/a/span[text()='Date Picker']");
+
 
     public SelectMenuPage clickSelectMenu() {
         scrollToElementJS(selectMenuItem);
@@ -15,4 +17,9 @@ public class WidgetsPage extends HomePage {
         return new SelectMenuPage();
     }
 
+    public DatePickerMenuPage clickDatePicker(){
+        scrollToElementJS(datePickerMenuItem);
+        click(selectMenuItem);
+        return new DatePickerMenuPage();
+    }
 }
