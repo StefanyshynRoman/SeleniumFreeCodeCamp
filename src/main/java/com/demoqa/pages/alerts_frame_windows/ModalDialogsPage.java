4825cec6c99bb28706dcaae73dc27ebfcdc2ae1e
roman <romanpz051@gmail.com>
2026-06-06 12:39:11 +0200

    0.0.3.3  Handle A Modal  Class

--- src/main/java/com/demoqa/pages/alerts_frame_windows/ModalDialogsPage.java
+++ src/main/java/com/demoqa/pages/alerts_frame_windows/ModalDialogsPage.java
@@ -1,5 +1,20 @@
 package com.demoqa.pages.alerts_frame_windows;
 
 
+import org.openqa.selenium.By;
+
 public class ModalDialogsPage extends Alerts_Frame_WindowsPage{
+    private By smallModalButton =By.id("showSmallModal");
+    private By smallModalText= By.xpath("//div[contains(text(),'small modal')]");
+    private By closeButton=By.id("closeSmallModal");
+
+    public void clickSmallModalButton(){
+        click(smallModalButton);
+    }
+    public String getSmallModalText(){
+        return find(smallModalText).getText();
+    }
+    public void clickCloseButton(){
+        click(closeButton);
+    }
 }
