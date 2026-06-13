a85fe5b82a8a5b6621ce4dede19fbbd7d8c210ac
roman <romanpz051@gmail.com>
2026-06-13 19:06:21 +0200

    0.0.3.4  Information  Alerts

--- src/main/java/utilities/SwitchToUtility.java
+++ src/main/java/utilities/SwitchToUtility.java
@@ -1,5 +1,16 @@
 package utilities;
 
 
-public class SwitchToUtility {
+import org.openqa.selenium.WebDriver;
+
+public class SwitchToUtility extends Utility {
+    private static WebDriver.TargetLocator switchTo() {
+        return driver.switchTo();
+    }
+    public static String getAlertText(){
+       return switchTo().alert().getText();
+    }
+    public static void acceptAlert(){
+        switchTo().alert().accept();
+    }
 }
--- src/test/java/part3_4/com/demoqa/tests/part4/alerts/AlertsTest.java
+++ src/test/java/part3_4/com/demoqa/tests/part4/alerts/AlertsTest.java
@@ -1,13 +1,23 @@
 package part3_4.com.demoqa.tests.part4.alerts;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import part3_4.com.demoqa.base.BaseTest;
+
+import static com.base.BasePage.delay;
+import static utilities.SwitchToUtility.*;
 
 
 public class AlertsTest extends BaseTest {
 
     @Test
-    public void testInformationAlert(){
-       var alertsPage= homePage.goToAlertsFramesWindowsCard().clickAlerts();
+    public void testInformationAlert() {
+        String expectedAlertText = "You clicked a button";
+        var alertsPage = homePage.goToAlertsFramesWindowsCard().clickAlerts();
+        alertsPage.clickInformationAlertButton();
+        delay(2000);
+        Assert.assertEquals(getAlertText(), expectedAlertText,
+                "\n Actual $ Expected Messages Do Not Match \n ");
+        acceptAlert();
     }
 }
