263f1d6c87ca53ac42fce8cd62fa0e3ec35e1384
roman <romanpz051@gmail.com>
2026-07-06 10:40:53 +0200

    0.0.3.7  Frames Introduction test up

--- src/main/java/com/demoqa/pages/alerts_frame_windows/FramesPage.java
+++ src/main/java/com/demoqa/pages/alerts_frame_windows/FramesPage.java
@@ -5,7 +5,14 @@ import org.openqa.selenium.By;
 
 public class FramesPage extends  Alerts_Frame_WindowsPage{
    private By textInFrame = By.id("sampleHeading");
+   private String iFrameBigBox="frame1";
+
+   private void switchToBigBox(){
+       driver.switchTo().frame(iFrameBigBox);
+   }
+
    public String getTextInBigFrame(){
+       switchToBigBox();
        String bigFrameText=find(textInFrame).getText();
        System.out.println("Big Frame Text: "+bigFrameText);
        return bigFrameText;
