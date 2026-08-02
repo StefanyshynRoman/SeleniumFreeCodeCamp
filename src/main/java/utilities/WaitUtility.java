1240dd60a7fbfd2257c21f01ad6cc735137e8ffa
roman <romanpz051@gmail.com>
2026-08-02 14:57:49 +0200

    0.0.4.2    Fluent Wait util

--- src/main/java/utilities/WaitUtility.java
+++ src/main/java/utilities/WaitUtility.java
@@ -2,7 +2,10 @@ package utilities;
 
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
@@ -13,5 +16,14 @@ public class WaitUtility extends Utility {
 
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(seconds));
         wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+    public static void fluentWaitUntilVisible(int second, By locator) {
+        FluentWait fluentWait = new FluentWait(driver)
+                .withTimeout(Duration.ofSeconds(second))
+                .pollingEvery(Duration.ofSeconds(500))
+                .ignoring(NoSuchElementException.class,
+                        StaleElementReferenceException.class);
+        fluentWait.until(ExpectedConditions.visibilityOfElementLocated(locator));
     }
 }
