08b9521b137526940780db02feba4caf576e2c7b
roman <romanpz051@gmail.com>
2026-08-09 08:49:56 +0200

    0.0.5.0    Simulate Mouse Movements

--- src/main/java/com/demoqa/pages/widgets/WidgetsPage.java
+++ src/main/java/com/demoqa/pages/widgets/WidgetsPage.java
@@ -9,8 +9,20 @@ import static utilities.JavaScriptUtility.scrollToElementJS;
 public class WidgetsPage extends HomePage {
     private By selectMenuItem = By.xpath("//li[@id='item-8']//span[text()='Select Menu']");
     private By datePickerMenuItem = By.xpath("//li[@id='item-2']/a/span[text()='Date Picker']");
+    private By sliderMenuItem = By.xpath("//li[@id='item-3']//span[text()='Slider']");
+    private By progressBarMenuItem = By.xpath("//li[@id='item-4']/span[text()='Progress Bar']");
 
+    public SliderPage clickSlider() {
+        scrollToElementJS(sliderMenuItem);
+        click(sliderMenuItem);
+        return new SliderPage();
+    }
 
+    public ProgressBarPage clickProgressBar() {
+        scrollToElementJS(progressBarMenuItem);
+        click(progressBarMenuItem);
+        return new ProgressBarPage();
+    }
     public SelectMenuPage clickSelectMenu() {
         scrollToElementJS(selectMenuItem);
         click(selectMenuItem);
---
+++ src/test/java/part3_4/com/demoqa/tests/part4/interactions/SliderTest.java
@@ -0,0 +1,21 @@
+package part3_4.com.demoqa.tests.part4.interactions;
+
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import part3_4.com.demoqa.base.BaseTest;
+
+public class SliderTest extends BaseTest {
+
+    @Test
+    public void testSliderResult() {
+        int x=180;
+        int y=0;
+        var sliderPage = homePage.goToWidgets().clickSlider();
+        sliderPage.moveSlider(x,y);
+        String actualValue =sliderPage.getSliderValue();
+        String expectedValue = "73";
+        Assert.assertEquals(actualValue,expectedValue,
+               " \n Actual $ Expected Text on Small Box  Do Not Match \n");
+    }
+}
